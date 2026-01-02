aa6e9a2e56c8e5478f7b6ca5fe0d83edea3b2661
Vincent Biret <vincentbiret@hotmail.com>
2026-01-02 15:01:31 -0500

    test: updates include patterns for better detection
    Signed-off-by: Vincent Biret <vincentbiret@hotmail.com>

--- @binkylabs/muzzle/vitest.config.ts
+++ @binkylabs/muzzle/vitest.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
     globals: true,
     environment: "node",
     testTimeout: 30000, // 30 seconds for slow TypeSpec compilation,
+    include: ["**/*.test.ts", "**/*.spec.ts"],
     exclude: ["**/node_modules/**", "**/dist/**"],
   },
 });
